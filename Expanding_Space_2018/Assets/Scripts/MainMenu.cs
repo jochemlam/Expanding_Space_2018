69f39e9a76b638b508524acc5eee87e271464fb5
jochemlam <24549@ma-web.nl>
2018-05-17 10:11:25 +0200

    updated mainmenu

--- Expanding_Space_2018/Assets/Scripts/MainMenu.cs
+++ Expanding_Space_2018/Assets/Scripts/MainMenu.cs
@@ -11,6 +11,8 @@ public class MainMenu : MonoBehaviour {
 
     public void ChangeScene (string scenename)
     {
+#pragma warning disable CS0618 // Type or member is obsolete
         Application.LoadLevel(scenename);
+#pragma warning restore CS0618 // Type or member is obsolete
     }
 }
