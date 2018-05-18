46eeeb064b5d4bc1dd8a6f19c09f7582df76b382
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2018-05-18 10:28:36 +0200

    bullet

--- Expanding_Space_2018/Assets/Scripts/DestroyBullet.cs
+++ Expanding_Space_2018/Assets/Scripts/DestroyBullet.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class DestroyBullet : MonoBehaviour {
 
-
+    private int BulletX;
     [SerializeField]
     private float TimeToDestroy = 5f;
 
 	void Start () {
         DestroyObject(this.gameObject, TimeToDestroy);
 	}
+
+   // BulletX = GameObject.FindGameObjectWithTag("Bullet").transform.position;
 }
--- Expanding_Space_2018/Assets/Scripts/MainMenu.cs
+++ Expanding_Space_2018/Assets/Scripts/MainMenu.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour {
 
-	public void EndGame()
-    {
-        Application.Quit();
-    }
+        public void EndGame()
+        {
+            Application.Quit();
+        }
 
-    public void ChangeScene (string scenename)
-    {
-#pragma warning disable CS0618 // Type or member is obsolete
-        Application.LoadLevel(scenename);
-#pragma warning restore CS0618 // Type or member is obsolete
-    }
+        public void ChangeScene(string scenename)
+        {
+        #pragma warning disable CS0618 // Type or member is obsolete
+            Application.LoadLevel(scenename);
+        #pragma warning restore CS0618 // Type or member is obsolete
+        }
+    
 }
