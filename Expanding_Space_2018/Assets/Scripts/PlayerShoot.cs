9235693ec3441dd021efa085aac10d9917430d32
jochemlam <24549@ma-web.nl>
2018-05-18 11:42:06 +0200

    set cooldown on bullet shoot and added laser

--- Expanding_Space_2018/Assets/Scripts/PlayerShoot.cs
+++ Expanding_Space_2018/Assets/Scripts/PlayerShoot.cs
@@ -10,23 +10,36 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField]
     private Rigidbody2D bullet;
 
+    private float Delay;
+
     //fire the bullet
     void Fire()
     {
-
+        
         Vector3 bulletpos = (transform.position + new Vector3(2f, 0, 0));
 
         Rigidbody2D bulletClone = (Rigidbody2D)Instantiate(bullet, bulletpos, transform.rotation);
         bulletClone.velocity = transform.right * bulletSpeed;
-        
+        Delay = 0.25f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Delay == 0f)
         {
+
             Fire();
+        }
+
+        if (Delay > 0)
+        {
+            
+            Delay -= 0.83f * Time.deltaTime;
+        }
+        if (Delay < 0)
+        {
+            Delay = 0;
         }
     }
 }
