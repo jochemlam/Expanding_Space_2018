ff3c61b411a0f3328ba67938de3f5ea1722d943d
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2018-05-18 13:33:01 +0200

    Star Dust

---
+++ Expanding_Space_2018/Assets/Scripts/sdScore.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sdScore : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            addScore();
+        }
+        
+    }
+
+    void addScore()
+    {
+        ScoreScript.ScoreValue += 5;
+    }
+}
