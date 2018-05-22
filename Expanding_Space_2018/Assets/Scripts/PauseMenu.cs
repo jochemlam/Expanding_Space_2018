3b88e3b734febb5828ea4d2cf326a265a4c59345
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2018-05-22 15:08:39 +0200

    dingen

---
+++ Expanding_Space_2018/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public void EndGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_WEBPLAYER
+                 Application.OpenURL(webplayerQuitURL);
+#else
+                 Application.Quit();
+#endif
+    }
+
+
+}
