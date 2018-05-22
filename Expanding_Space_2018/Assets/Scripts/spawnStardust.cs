3a3ca5bf59db3e2e4e9657c3fb62eec5e468299f
jochemlam <24549@ma-web.nl>
2018-05-22 10:26:09 +0200

    updated stardust

--- Expanding_Space_2018/Assets/Scripts/spawnStardust.cs
+++ Expanding_Space_2018/Assets/Scripts/spawnStardust.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class spawnStardust : MonoBehaviour {
 
+    public GameObject Stardust;
+    private int minInt = 1;
+    private int maxInt = 10;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Bullet")
@@ -12,5 +16,13 @@ public class spawnStardust : MonoBehaviour {
             Destroy(this.gameObject);
         }
 
+    }
+
+    void RandomDustSpawn()
+    {
+        if (Random.Range(minInt, maxInt) < 2)
+        {
+            Instantiate(Stardust, transform.parent.position, Quaternion.identity);
+        }
     }
 }
