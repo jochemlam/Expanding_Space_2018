2ebed36154d328f48a0909fb8f2738404edf7d50
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2018-05-30 11:51:06 +0200

    sound

---
+++ Expanding_Space_2018/Assets/Scripts/SoundScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundScript : MonoBehaviour {
+
+    public AudioClip ShootSoundClip;
+
+    public AudioSource SoundManager;
+
+	// Use this for initialization
+	void Start () {
+        SoundManager.clip = ShootSoundClip;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SoundManager.Play();
+        }
+	}
+}
