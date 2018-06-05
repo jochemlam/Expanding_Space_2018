9bd34bc42106e27cb15fd3de03da6a1d9f260503
LAPTOP\Jarno van Lierop <jarnovanlierop@gmail.com>
2018-06-05 14:50:12 +0200

    Slow Down works

---
+++ Expanding_Space_2018/Assets/Delete_Objects.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete_Objects : MonoBehaviour {
+
+    private void Awake()
+    {
+        Invoke("DestroyThisObject", 1);
+    }
+    public void DestroyThisObject()
+    {
+        Destroy(this.gameObject);
+    }
+}
---
+++ Expanding_Space_2018/Assets/SlowDown.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowDown : MonoBehaviour {
+
+    public float multiplier = 1.4f;
+    public float duration = 1f;
+
+    public GameObject pickupEffect;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            StartCoroutine( Pickup(other) );
+        }
+    }
+
+    IEnumerator Pickup(Collider2D Player)
+    {
+        Debug.Log("Power up picked up");
+        Instantiate(pickupEffect, transform.position, transform.rotation);
+
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+
+
+        Player.transform.localScale *= multiplier;
+
+        yield return new WaitForSeconds(duration);
+
+        Player.transform.localScale /= multiplier;
+
+        Destroy(gameObject);
+
+
+    }
+}
